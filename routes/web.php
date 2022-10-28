1a946f06ed1814fd95a22ed357ec18ac43cbe01c
David7ce <david7ce@outlook.com>
2022-10-28 14:00:40 +0100

    remove welcome and return view of login user

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if (!auth()->user()) {
+        return redirect(route('login'));
+    }
+    return view('front-page');
 });
 
 Route::get('/front-page', function () {
