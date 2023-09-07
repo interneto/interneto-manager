6f9fee8a5d259dde182fe21d825e3a2516600236
David7ce <david7ce@outlook.com>
2023-09-08 00:25:43 +0100

    delete comments in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,68 +22,13 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
+        //     'password' => bcrypt('password123'),
+        //     'avatar' => 'default-avatar.png',
         // ]);
 
-        /*
-        // Tags of audio
-        $audio_tag = \App\Models\Tag::factory()->create([
-            "name" => "Audio",
-        ]);
-
-        $audio_links = \App\Models\Link::factory(10)->create();
-        foreach ($audio_links as $link) {
-            $link->tags()->attach($audio_tag);
-        }
-
-        // Tags of image
-        $image_tag = \App\Models\Tag::factory(10)->create([
-            "name" => "Image",
-        ]);
-
-        $image_links = \App\Models\Link::factory(10)->create();
-        foreach ($image_links as $image) {
-            $link->tags()->attach($image_tag);
-        }
-
-        // Tags of text
-        $text_tag = \App\Models\Tag::factory(10)->create([
-            "name" => "Text",
-        ]);
-        foreach ($image_links as $image) {
-            $link->tags()->attach($image_tag);
-        }
-
-        // Tag of video
-        $video_tag = \App\Models\Tag::factory()->create([
-            "name" => "Video",
-        ]);
-
-        $video_links = \App\Models\Link::factory(10)->create();
-        foreach ($video_links as $link) {
-            $link->tags()->attach($video_tag);
-        }
-        */
-
-       // Tags of audio
-        $audio_tag = \App\Models\Tag::factory()->create([
-            "name" => "Audio",
-        ]);
-        
-        /*
-        DB::table('types')->insert([
-            'name' => Str::random(),    // audio, image, text, video
-        ]);
-        */
-
         // User
-
         // Directory
-
-
-        // links
-
-
+        // Links
         // Types
-
     }
 }
